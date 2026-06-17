ab4ef7904d46ca49637a8aabbb059984b06a84ab
Hoss Maghsoudi <mahroozware@gmail.com>
2026-06-16 23:19:23 -0700

    fix blog layout desktop and mobile

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -164,7 +164,7 @@ export default async function BlogPostPage({
 
       {/* ── BODY ── */}
       <div className="blog-post-layout">
-        <div className="blog-post-container">
+        <div className="blog-post-container" style={{ width: '100%' }}>
 
           {/* Article */}
           <article className="blog-post-article">
